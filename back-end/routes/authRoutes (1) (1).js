410952c458c06f7c75e018241969f97f07781628
NKDON18 <nawarajgaming@gmail.com>
2025-11-30 08:48:04 +0545

    dd

---
+++ back-end/routes/authRoutes (1) (1).js
@@ -0,0 +1,12 @@
+import express from "express";
+import { register, login, changePassword } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/login", login);
+
+// Route for changing password
+router.post("/change-password", changePassword);
+
+export default router;
